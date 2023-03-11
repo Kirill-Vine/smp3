951d68f91cb6df843d79009247b65564489632df
Churg <70928683+Churg@users.noreply.github.com>
2023-03-10 23:51:23 -0500

    committing src
    adding src

--- src/main/java/project3/TuitionManagerController.java
+++ src/main/java/project3/TuitionManagerController.java
@@ -1,14 +1,17 @@
 package project3;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 
 public class TuitionManagerController {
+
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void testEvent() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
